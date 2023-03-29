72ceebba35a0477d57d6dd23ce40f946c68b9efc
Michael <mihay0113@gmail.com>
2023-03-29 14:24:18 +0300

    added thunk

--- src/components/Position.tsx
+++ src/components/Position.tsx
@@ -1,15 +1,17 @@
-import { useDispatch } from 'react-redux';
-import { useAppSelector } from '../app/hooks';
-import { actions as positionActions } from '../features/position';
+import { Dispatch } from 'redux';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import  * as positionActions from '../features/position';
 
 export const Position = () => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { x, y } = useAppSelector(state => state.position);
 
   const moveLeft = () => dispatch(positionActions.moveLeft());
   const moveRight = () => dispatch(positionActions.moveRight());
   const moveUp = () => dispatch(positionActions.moveUp());
   const moveDown = () => dispatch(positionActions.moveDown());
+
+  const dance = () => dispatch(positionActions.doACircle(500));
 
   const transformValue = `translate(${x * 100}%, ${y * 100}%)`;
 
@@ -31,7 +33,7 @@ export const Position = () => {
         </div>
 
         <div className="field">
-          <div className="track" style={{ transform: transformValue }}>
+          <div className="track" style={{ transform: transformValue }} onClick={dance}>
             {x + y}
           </div>
         </div>
